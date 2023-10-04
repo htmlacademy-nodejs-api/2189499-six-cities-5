5238f86debe34885d30ca1269adb4e86cdc18025
Alexandr <alexandr@MacBook-Pro-Alexandr.local>
2023-10-04 14:12:24 -0700

    Закончили с добавлением типов

---
+++ src/shared/types/rent.type.ts
@@ -0,0 +1,24 @@
+import { HouseType } from "./house.type.js"
+import { Comfort } from "./comfort.type.js"
+import { User } from "./users.type.js"
+
+export type Rent = {
+  name: string,
+  description: string,
+  date: string,
+  town: string,
+  mainImage: string,
+  images: string[],
+  isPremium: boolean,
+  isFavorite: boolean,
+  rating: number,
+  houseType: HouseType,
+  roomCount: number,
+  guestCount: number,
+  price: number,
+  comfort: Comfort[],
+  author: User,
+  commentCount: number,
+  latitude: number;
+  longitude: number;
+}
