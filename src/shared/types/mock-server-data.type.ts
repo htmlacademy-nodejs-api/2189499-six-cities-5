9d93c3d239aa4c1f6ef66a1d3715e6c1d8f17f41
Alexandr <alexandr@MacBook-Pro-Alexandr.local>
2023-10-09 20:05:31 -0700

    создал интерфейс для генерации, а также класс

--- src/shared/types/mock-server-data.type.ts
+++ src/shared/types/mock-server-data.type.ts
@@ -1,22 +1,22 @@
 import { HouseType } from "./house.type.js";
 
 export type MockServerData = {
-  name: string,
-  description: string,
-  date: Date,
-  town: string,
-  mainImage: string,
+  name: string[],
+  description: string[],
+  date: Date[],
+  town: string[],
+  mainImage: string[],
   images: string[],
-  isPremium: boolean,
-  isFavorite: boolean,
-  rating: number,
-  houseType: HouseType,
-  roomCount: number,
-  guestCount: number,
-  price: number,
+  isPremium: boolean[],
+  isFavorite: boolean[],
+  rating: number[],
+  houseType: HouseType[],
+  roomCount: number[],
+  guestCount: number[],
+  price: number[],
   comfort: string[],
-  author: string,
-  commentCount: number,
-  latitude: number;
-  longitude: number;
+  author: string[],
+  commentCount: number[],
+  latitude: number[];
+  longitude: number[];
 };
