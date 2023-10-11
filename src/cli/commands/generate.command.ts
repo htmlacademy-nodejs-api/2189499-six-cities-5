a70b971f7a9cc49ec2e8a635f36b6145a2770d88
Alexandr <alexandr@MacBook-Pro-Alexandr.local>
2023-10-10 22:20:12 -0700

    добавили генератор внутри generate.command.ts

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,6 +1,8 @@
 import got from "got";
 import { Command } from "./command.interface.js";
-import { MockServerData } from "../../shared/types/index.js";
+import { MockServerData, TSVFileReader } from "../../shared/types/index.js";
+import { TSVOfferGenerator } from "../../shared/types/libs/offer-generator.interface.ts/tsv-offer-generator.js";
+import { appendFile } from "node:fs/promises";
 
 export class GenerateCommand implements Command {
   
@@ -18,12 +20,26 @@ export class GenerateCommand implements Command {
     return '--generate';
   }
 
+  public async write(filepath: string, offerCount: number){
+    const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+
+    for (let i = 0; i < offerCount; i++) {
+      await appendFile(
+        filepath,
+        `${tsvOfferGenerator.generate()}\n`,
+        {encoding: 'utf-8'}
+      );
+    }
+  }
+
   public async execute(...parameters: string[]): Promise<void> {
     const [count, filepath, url] = parameters;
     const offerCount = Number.parseInt(count, 10);
 
     try {
       await this.load(url);
+      await this.write(filepath, offerCount);
+      console.info(`File ${filepath} was created!`);
     } catch (error: unknown) {
       console.log('Can\'t generate data');
 
@@ -31,6 +47,5 @@ export class GenerateCommand implements Command {
         console.error(error.message);
       }
     }
-    //код для получения данных с сервера
   }
 }
